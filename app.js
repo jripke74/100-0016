61e71c62d39cc595c0ef673406f6341ab48cd2c1
Jeff Ripke <jripke74@gmail.com>
2022-01-30 09:07:43 -0800

    add querySelector()

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 let anchorElement = document.getElementById('external-link');
 anchorElement.href = 'https://google.com';
+
+anchorElement = document.querySelector('a');
+anchorElement.href = 'https://jeffripke.com';
