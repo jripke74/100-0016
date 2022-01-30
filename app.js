c41e018dd485a354078a9a442bdfbde1ec22a67a
Jeff Ripke <jripke74@gmail.com>
2022-01-30 09:19:53 -0800

    DOM traversing

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+let h1 = document.body.children[0];
+console.log(h1.nextSibling.nextSibling);
+
+let headingOne = document.getElementById('main-heading');
+let secondP = document.querySelector('#second-p');
+console.log(secondP);
+secondP.textContent = 'changed!!!';
+
 let anchorElement = document.getElementById('external-link');
 anchorElement.href = 'https://google.com';
 
