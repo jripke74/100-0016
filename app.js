138c6c1ce380f6472433de4508a4021fa4096c4c
Jeff Ripke <jripke74@gmail.com>
2022-01-31 07:52:11 -0800

    insert a element in app.js

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 let h1 = document.body.children[0];
 console.log(h1.nextSibling.nextSibling);
 
-let headingOne = document.getElementById('main-heading');
-let secondP = document.querySelector('#second-p');
-console.log(secondP);
-secondP.textContent = 'changed!!!';
+// let headingOne = document.getElementById('main-heading');
+// let secondP = document.querySelector('#second-p');
+// console.log(secondP);
+// secondP.textContent = 'changed!!!';
 
-let anchorElement = document.getElementById('external-link');
-anchorElement.href = 'https://google.com';
+// let anchorElement = document.getElementById('external-link');
+// anchorElement.href = 'https://google.com';
 
 anchorElement = document.querySelector('a');
 anchorElement.href = 'https://jeffripke.com';
+
+// ADD AN ELEMENT
+// 1. Creae the new element
+
+const newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'https://google.com';
+newAnchorElement.textContent = 'This leads to Google';
+
+// 2. Get access to the parent element that should hold the new element
+
+const firstParagraph = document.querySelector('p');
+
+// 3. Insert the new element into the parent elelment content
+
+firstParagraph.append(newAnchorElement);
