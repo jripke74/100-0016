b01caeaf77bc0920e30642d2e90d4aff6c2e7c51
Jeff Ripke <jripke74@gmail.com>
2022-02-01 10:34:03 -0800

    add class warning to productNameInnputElement

--- demo.js
+++ demo.js
@@ -3,6 +3,8 @@ const remainingCharsElement = document.getElementById('remaining-chars');
 
 const maxAllowedChars = productNameInputElement.maxLength;
 
+remainingCharsElement.style.color = 'rgb(0, 255, 0)';
+
 function updateRemainingCharactors(event) {
   const enteredText = event.target.value;
   const enteredTextLength = enteredText.length;
@@ -10,6 +12,11 @@ function updateRemainingCharactors(event) {
   const remainingCharacters = maxAllowedChars - enteredTextLength;
 
   remainingCharsElement.textContent = remainingCharacters;
+
+  if (remainingCharacters <= 10) {
+    remainingCharsElement.classList.add('warning');
+    productNameInputElement.classList.add('warning');
+  }
 }
 
 productNameInputElement.addEventListener('input', updateRemainingCharactors);
