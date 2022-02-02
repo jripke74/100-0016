499ba8c21fc29d76476ca91cb0cbe0f59677335e
Jeff Ripke <jripke74@gmail.com>
2022-02-02 08:42:31 -0800

    add display user data

--- loops.js
+++ loops.js
@@ -29,3 +29,28 @@ function highlightLinks() {
 }
 
 highlightLinkskButtonElement.addEventListener('click', highlightLinks);
+
+// Display user data
+
+const dummyUserData = {
+  firstName: 'Douglas',
+  lastName: 'Ripke',
+  age: 17,
+}
+
+const displayUserDataButtonElement = document.querySelector('#user-data button');
+
+function displayUserData() {
+  const outputDataElement = document.getElementById('output-user-data');
+
+  outputDataElement.innerHTML = '';
+
+  for (const key in dummyUserData) {
+    const newUserDataListItemElement = document.createElement('li');
+    const outputText = key.toUpperCase() + ': ' + dummyUserData[key];
+    newUserDataListItemElement.textContent = outputText;
+    outputDataElement.append(newUserDataListItemElement);
+  }
+}
+
+displayUserDataButtonElement.addEventListener('click', displayUserData);
